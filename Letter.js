97562f6e525ccb93d828a61a65aabfba614cae16
Caitlin Everett <caitlineverett@gmail.com>
2018-05-12 09:00:58 -0500

    commented unfinished code

---
+++ Letter.js
@@ -0,0 +1,17 @@
+// The Letter constructor is responsible for displaying either an underscore or the underlying character for each letter in the word
+
+// If a character is not a number or a letter, make it visible right away
+// Save the underlying character
+
+
+// prototypes are optional
+
+// Returns either an underscore or the underlying character depending on `this.visible`
+//    because we call this function toString, when we call `this.letters.join` in
+//    Word.js, JavaScript automatically uses the value we return here
+
+
+// Accepts a user's guess
+
+
+// Otherwise return false
--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-var Game = require("./lib/Game");
 
 // Initialize a new Game object
-var game = new Game();
 
 // Start playing
-game.play();
